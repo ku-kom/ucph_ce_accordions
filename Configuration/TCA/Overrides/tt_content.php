fa2de0caaafee90042ab706a1064b1cc46c06ef1
Nanna Ellegaard <nel@adm.ku.dk>
2023-08-18 13:44:03 +0200

    Remove bodytext from accordion container

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -51,8 +51,7 @@ call_user_func(function ($extKey ='ucph_ce_accordions', $contentType ='ucph_ce_a
             'showitem' => '
             --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
                 --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
-                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers,
-                bodytext;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:bodytext_formlabel,
+                header; LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:internal_header,
             --div--;LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:options,
                 pi_flexform;LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:advanced,
             --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
